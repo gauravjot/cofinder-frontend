8a98987d0b2cdae7675fcb3ff2805b64a1535811
Gauravjot Garaya <gauravjot@outlook.com>
2023-06-03 13:34:34 -0700

    added endpoints to env file

--- src/server_eps.ts
+++ src/server_eps.ts
@@ -1,41 +1,57 @@
-// UFV API Endpoints
-// export const sectionsEP = (term: string, encodedCRNs: string | null = null): string => {
-// 	if (encodedCRNs) {
-// 		return `https://cf-api.cofinder.ca/sections?term=${term}&crns=${encodedCRNs}`;
-// 	}
-// 	return `https://cf-api.cofinder.ca/sections?term=${term}`;
-// };
-// export const EP_TERMS = "https://cf-api.cofinder.ca/terms";
-// export const subjectsEP = (term: string) => {
-// 	return `https://cf-api.cofinder.ca/subjects?term=${term}`;
-// };
-// export const coursesEP = (term: string) => {
-// 	return `https://cf-api.cofinder.ca/courses?term=${term}`;
-// };
-// export const instructorsEP = (term: string) => {
-// 	return `https://cf-api.cofinder.ca/instructors?term=${term}`;
-// };
-// export const seatsEP = (term_name: string | number, crn: string | number) => {
-// 	return `https://cf-api.cofinder.ca/seats?term_name=${term_name}&crn=${crn}`;
-// };
-
-/* DEV */
 export const sectionsEP = (term: string, encodedCRNs: string | null = null): string => {
 	if (encodedCRNs) {
-		return `http://localhost:8000/api/${term}/sections/${encodedCRNs}/`;
+		console.log(process.env.REACT_APP_INSERTION_IDENTIFIER);
+		return (
+			process.env.REACT_APP_SPECIFIC_SECTION_EP?.replace(
+				process.env.REACT_APP_INSERTION_IDENTIFIER || "{}",
+				term
+			)?.replace(process.env.REACT_APP_INSERTION_IDENTIFIER || "{}", encodedCRNs) ||
+			""
+		);
 	}
-	return `http://localhost:8000/api/${term}/sections/`;
+	return (
+		process.env.REACT_APP_SECTION_EP?.replace(
+			process.env.REACT_APP_INSERTION_IDENTIFIER || "{}",
+			term
+		) || ""
+	);
 };
-export const EP_TERMS = "http://localhost:8000/api/terms/";
+
+export const EP_TERMS: string = process.env.REACT_APP_TERM_EP || "";
+
 export const subjectsEP = (term: string) => {
-	return `http://localhost:8000/api/${term}/subjects/`;
+	return (
+		process.env.REACT_APP_SUBJECTS_EP?.replace(
+			process.env.REACT_APP_INSERTION_IDENTIFIER || "{}",
+			term
+		) || ""
+	);
 };
+
 export const coursesEP = (term: string) => {
-	return `http://localhost:8000/api/${term}/courses/`;
+	return (
+		process.env.REACT_APP_COURSES_EP?.replace(
+			process.env.REACT_APP_INSERTION_IDENTIFIER || "{}",
+			term
+		) || ""
+	);
 };
+
 export const instructorsEP = (term: string) => {
-	return `http://localhost:8000/api/${term}/instructors/`;
+	return (
+		process.env.REACT_APP_INSTRUCTORS_EP?.replace(
+			process.env.REACT_APP_INSERTION_IDENTIFIER || "{}",
+			term
+		) || ""
+	);
 };
+
 export const seatsEP = (term_name: string | number, crn: string | number) => {
-	return `http://localhost:8000/api/section/seats/${term_name}/${crn}/`;
+	return (
+		process.env.REACT_APP_SEATS_EP?.replace(
+			process.env.REACT_APP_INSERTION_IDENTIFIER || "{}",
+			term_name.toString()
+		)?.replace(process.env.REACT_APP_INSERTION_IDENTIFIER || "{}", crn.toString()) ||
+		""
+	);
 };
